84bc8ee685be9e27a2a981e650be137073654972
Ray Cai Flanagan <hjkareus@gmail.com>
2023-07-01 11:29:08 -0400

    Editor

--- src/components/summaryCard/index.tsx
+++ src/components/summaryCard/index.tsx
@@ -3,7 +3,7 @@ import { AiOutlineCloseCircle } from 'react-icons/ai';
 import classes from './styles.module.css';
 
 type SummaryCardProps = {
-    summary: string;
+    card: Card;
     onDelete?: () => void; // ! Make non-optional
 }
 
@@ -11,13 +11,13 @@ export default function SummaryCard(props: SummaryCardProps) {
     return (
         <div className={ classes.container }>
             <div className={ classes.header }>
-                <h1>{ props.summary }</h1>
+                <h1>{ props.card.title }</h1>
 
                 <AiOutlineCloseCircle onClick={ props.onDelete } fontSize={ 20 } className={ classes.closeBtn } />
             </div>
 
             <div onClick={ props.onDelete } className={ classes.cardContent }>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ex placeat modi pariatur rerum facilis vitae tempora dicta. Illum, reiciendis, ea voluptas quo repellat excepturi consequatur, facere mollitia deserunt at saepe laborum maxime in et iusto dicta magnam. Ipsa corporis numquam asperiores temporibus alias saepe blanditiis quaerat consequatur nisi aspernatur voluptatum repellat tempora reprehenderit libero soluta quidem ipsum quos, sapiente molestias, totam unde debitis! Repellendus ipsum sint labore veritatis fuga quod nemo neque facilis eius sit eos dolores voluptate iste enim vel quidem cupiditate exercitationem, amet sunt iure, laborum deserunt. Eius iure esse tenetur quasi libero? Voluptatum assumenda aliquid labore.</p>
+                <p>{ props.card.summary }</p>
             </div>
         </div>
     );
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,2 +1,9 @@
 declare module '*.css';
 declare module '*.svg';
+
+declare interface Card {
+    title: string;
+    summary: string;
+    start: number;
+    end: number;
+}
